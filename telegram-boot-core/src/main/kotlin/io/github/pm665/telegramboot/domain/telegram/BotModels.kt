1c03e2ef00fe9a7b1594fa0e6f88246a7c03a0c9
Pavel Minin <pavel.minin@actabl.com>
2025-09-29 19:13:29 -0400

    bot models

--- telegram-boot-core/src/main/kotlin/io/github/pm665/telegramboot/domain/telegram/BotModels.kt
+++ telegram-boot-core/src/main/kotlin/io/github/pm665/telegramboot/domain/telegram/BotModels.kt
@@ -1,19 +1,58 @@
 package io.github.pm665.telegramboot.domain.telegram
 
 data class Bot(
-    val botUsername: String,
+    val botUsername: String, // identifyiable by
     val botToken: String,
     val enabled: Boolean,
 )
 
 data class BotUser(
-    val botUserId: Long,
-    val botUserName: String,
-    val registered: Boolean,
+    val botUserId: Long, // identifyiable by
+    val username: String,
+    val active: Boolean = false,
 )
 
 data class BotChat(
-    val botUsername: String,
-    val botUserId: Long,
+    val botUsername: String, // identifyiable by
+    val botUserId: Long, // identifyiable by
     val botChatId: Long,
 )
+
+data class UserRole(
+    val botUsername: String?, // identifyiable by
+    val botUserId: Long, // identifyiable by
+    val role: Role,
+)
+
+enum class Role {
+    SUPER_ADMIN,
+    BOT_ADMIN,
+    USER,
+}
+
+data class Command(
+    val command: String, // identifyiable by
+    val botUsername: String, // identifyiable by
+    val enabled: Boolean, // identifyiable by
+    val type: CommandType,
+    val action: String,
+    val label: String,
+    val description: String,
+)
+
+data class Menu(
+    val command: String, // identifyiable by
+    val botUsername: String, // identifyiable by
+    val enabled: Boolean, // identifyiable by
+    val parent: String? = null,
+    val section: Int = 0,
+    val row: Int = 0,
+    val order: Int = 0,
+)
+
+enum class CommandType {
+    COMMAND,
+    INFO_LINK,
+    EXTERNAL,
+    MENU,
+}
